841b11d0c1fc096fc6d50eaeed810086fc33fea1
LEONARDO ALEXANDRE ALVES DOS SANTOS JUNIOR <leonardo.aasjunior@senacsp.edu.br>
2025-10-03 16:52:45 -0300

    criação da função que verifica estoque minimo e exibe filiais

--- processa.php
+++ processa.php
@@ -10,6 +10,20 @@ function calcularEstoqueTotal($q1, $q2, $q3) {
     return $q1 + $q2 + $q3;
 }
 
+function verificarEstoqueBaixo($nome, $q1, $q2, $q3, $limite = 30) {
+    $filiais = [];
+    if ($q1 < $limite) $filiais[] = "Filial 1";
+    if ($q2 < $limite) $filiais[] = "Filial 2";
+    if ($q3 < $limite) $filiais[] = "Filial 3";
+
+    if (count($filiais) > 0) {
+        $listaFiliais = implode(", ", $filiais);
+        return "Estoque Baixo - Filiais: $listaFiliais";
+    } else {
+        return "Estoque OK";
+    }
+}
+
 for ($i = 1; $i <= 5; $i++) {
     $nome = $_POST["produto{$i}_nome"];
     $f1 = (int) $_POST["produto{$i}_filial1"];
@@ -24,11 +38,16 @@ for ($i = 1; $i <= 5; $i++) {
     }
 
     $total = calcularEstoqueTotal($f1, $f2, $f3);
+    $situacao = verificarEstoqueBaixo($nome, $f1, $f2, $f3, 30);
+
     echo "<h3>Produto $i: $nome</h3>";
     echo "Filial 1: $f1 unidades<br>";
     echo "Filial 2: $f2 unidades<br>";
     echo "Filial 3: $f3 unidades<br>";
-    echo "<b>Total:</b> $total unidades<br><br>";
+    echo "<b>Total:</b> $total unidades<br>";
+    if ($situacao != "Estoque OK") {
+        echo "<p style='color:red'><b>$situacao</b></p>";
+    }
+    echo "<br>";
 }
 ?>
-
